68002f4be2276f49eaa6ae34a36fbcdad8ada124
hanaessam <hanaessamsayed@gmail.com>
2025-11-29 20:21:50 +0200

    add app instance to the private network

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,18 +1,19 @@
-# Security Group for Private EC2 (Application) for SSH from Bastion and App traffic
+# Security Group for Private Instances (Allow SSH from Bastion and App Port)
 resource "aws_security_group" "private_sg" {
   name        = "${var.env_name}-private-sg"
   description = "App server SG"
   vpc_id      = var.vpc_id
 
+  # Allow SSH only from Bastion SG
   ingress {
     description = "SSH from Bastion"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     security_groups = [var.public_sg_id]
-
   }
 
+  # Allow port 3000 only from VPC
   ingress {
     description = "App port"
     from_port   = 3000
@@ -33,36 +34,35 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
-
-# RSA key of size 4096 bits
+# Generate a TLS private key for the Bastion host
 resource "tls_private_key" "bastion_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-
-# Key Pair for Bastion Host
+# Create an AWS Key Pair using the generated public key
 resource "aws_key_pair" "bastion_key_pair" {
   key_name   = "${var.env_name}-bastion-key"
   public_key = tls_private_key.bastion_key.public_key_openssh
-
-  depends_on = [tls_private_key.bastion_key]
-
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.bastion_key.private_key_pem}' > ${var.env_name}_bastion_key.pem && chmod 400 ${var.env_name}_bastion_key.pem"
-  }
 }
 
-# Bastion Host EC2 Instance
+# Save the private key to a local file
+resource "local_file" "bastion_pem" {
+  content         = tls_private_key.bastion_key.private_key_pem
+  filename        = "${path.module}/${var.env_name}_bastion_key.pem"
+  file_permission = "0400"
+}
+
+
+# Create a Bastion Host in the Public Subnet
 resource "aws_instance" "bastion" {
   ami                    = "ami-0fa3fe0fa7920f68e"
   instance_type          = "t2.micro"
-  
+
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [var.public_sg_id]
 
   key_name = aws_key_pair.bastion_key_pair.key_name
-
   associate_public_ip_address = true
 
   tags = {
@@ -70,8 +70,31 @@ resource "aws_instance" "bastion" {
   }
 }
 
+# Application EC2 in Private Subnet
+resource "aws_instance" "app" {
+  ami           = "ami-0fa3fe0fa7920f68e" 
+  instance_type = "t2.micro"
 
+  subnet_id              = var.private_subnet_id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
 
+  # Use same TLS key as bastion
+  key_name = aws_key_pair.bastion_key_pair.key_name
 
+  # NO public IP for private subnet servers
+  associate_public_ip_address = false
 
+  # Optional: install Node.js
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y gcc-c++ make
+              curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+              yum install -y nodejs
+              EOF
+
+  tags = {
+    Name = "${var.env_name}-app"
+  }
+}
 
--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -4,3 +4,7 @@ output "private_sg_id" {
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+
+output "app_private_ip" {
+  value = aws_instance.app.private_ip
+}
